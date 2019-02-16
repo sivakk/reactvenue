0918673e44d5d80aa3a34a65cf7479bfe377de68
sivakk <ksiva1355@gmail.com>
2019-02-16 16:29:56 +0530

    filter

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header"/>>
+      <div >
+ 
         <h1>dlh,ljkf</h1>
           
       </div>
